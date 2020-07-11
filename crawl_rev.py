03f659926f198b551db19f23ebb90d011d18cf5d
ChenliTW <ab0897867564534231@gmail.com>
2020-07-11 23:40:00 +0800

    upd readme

---
+++ crawl_rev.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import datetime
+import xmltodict
+import json
+
+def get_nexttrain(station_ID):
+    headers={'Content-Type': 'text/xml','SOAPAction':'http://tempuri.org/GetNextTrain2'}
+    data="""<?xml version="1.0" encoding="utf-8"?>
+            <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+                <soap:Body>
+                    <GetNextTrain2 xmlns="http://tempuri.org/">
+                    <stnid>{}</stnid>
+                    </GetNextTrain2>
+                </soap:Body>
+            </soap:Envelope>""".format(station_ID)
+    res=requests.post("http://ws.metro.taipei/trtcappweb/Traintime.asmx",headers=headers,data=data)
+    return BeautifulSoup(res.text,'xml')
+
+def car_stat(trip_no):
+    headers={'Content-Type': 'text/xml','SOAPAction':'http://tempuri.org/GetCartDetailbyTripID'}
+    data="""<?xml version="1.0" encoding="utf-8"?>
+            <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+            <soap:Body>
+                <GetCartDetailbyTripID xmlns="http://tempuri.org/">
+                <strCW>{}</strCW>
+                </GetCartDetailbyTripID>
+            </soap:Body>
+            </soap:Envelope>""".format(trip_no)
+    res=requests.post("http://ws.metro.taipei/trtcappweb/CartWeight.asmx",headers=headers,data=data)
+    return BeautifulSoup(res.text,'xml')
+
+out={}
+trip_no=""
+
+while 1:
+    print(get_nexttrain("097").find_all("Detail")[1])
+    if get_nexttrain("097").find_all("Detail")[1]['tripno']!="":
+        trip_no=get_nexttrain("097").find_all("Detail")[1]['tripno']
+        break
+    time.sleep(1)
+
+print()
+print(trip_no)
+
+station=""
+while 1:
+    res=xmltodict.parse(str(car_stat(trip_no).find("Detail")))["Detail"]
+    if station!=res["@StationID"]:
+        station=res["@StationID"]
+        out[res["@StationID"]]=res
+        print(station)
+
+        f=open(str(trip_no),"w+")
+        f.write(json.dumps(out))
+        f.close()
+    time.sleep(1)
+
+    
